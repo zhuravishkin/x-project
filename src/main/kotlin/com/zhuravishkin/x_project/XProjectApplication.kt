f7ea5a909b7f274c02508d6dcf4704f62e38e0fa
aleksey.zhuravishkin <zhuravishkin@gmail.com>
2025-11-16 13:37:51 +0300

    init

---
+++ src/main/kotlin/com/zhuravishkin/x_project/XProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.zhuravishkin.x_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XProjectApplication
+
+fun main(args: Array<String>) {
+    runApplication<XProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/zhuravishkin/x_project/XProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.zhuravishkin.x_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class XProjectApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
